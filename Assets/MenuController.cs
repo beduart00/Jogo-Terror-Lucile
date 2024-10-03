99b57ca5c0e0f46a73e12c49f470d4d4863e60ca
beduart00 <165905670+beduart00@users.noreply.github.com>
2024-10-03 14:26:44 -0300

    scripts menu fade

--- Assets/MenuController.cs
+++ Assets/MenuController.cs
@@ -1,12 +1,30 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class MenuController : MonoBehaviour
 {
+
+public string nomeDaFaseParaCarregar;
+   public GameObject FadeCanvas;
+
+public void click(){
+
+    StartCoroutine(fadeCarregaFase());
+}
+
+public IEnumerator fadeCarregaFase(){
+
+FadeCanvas.GetComponent<Animator>().Play("FadeIn");
+ 
+
+yield return new WaitForSeconds(2f);
+   SceneManager.LoadScene(nomeDaFaseParaCarregar);
+
+}
+ 
+
     // Método para carregar a cena
-    public void LoadScene(string sceneName)
-    {
-        SceneManager.LoadScene(sceneName);
-    }
+
 }
