732bced9dbb4fc737fb3384f6971592984abc5f5
beduart00 <beatriz84268@gmail.com>
2024-10-31 15:49:38 -0300

    Teste trigger 4

--- Assets/trigger 4.cs
+++ Assets/trigger 4.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class Trigger_4 : MonoBehaviour
 {
@@ -11,29 +12,38 @@ public class Trigger_4 : MonoBehaviour
 
     private bool diarioAberto = false; // Para controlar o estado do diário
 
+    private bool estaColindo;
+    public GameObject btnFeedback;
+
+    
+
     void Start()
     {
         // Esconder o Canvas do diário no início
         diarioCanvas.SetActive(false);
+          btnFeedback= GameObject.Find("feedback");
     }
 
     private void Update()
     {
-        // Verifica se o diário está aberto e se a tecla de espaço foi pressionada
-        if (diarioAberto && Input.GetKeyDown(KeyCode.Space))
-        {
-            FecharDiario();
-        }
-        // Verifica se o jogador está perto e se a tecla de espaço foi pressionada para abrir o diário
-        else if (!diarioAberto && textoDaNarrativa.activeSelf && Input.GetKeyDown(KeyCode.Space))
+
+      // Verifica se o jogador está perto e se a tecla de espaço foi pressionada para abrir o diário
+        if (!diarioAberto && Input.GetKeyDown(KeyCode.Space)&&estaColindo==true)
         {
             AbrirDiario();
         }
+
+        // Verifica se o diário está aberto e se a tecla de espaço foi pressionada
+       else if (diarioAberto && Input.GetKeyDown(KeyCode.Space)&&estaColindo==true)
+        {
+            FecharDiario();
+        }
+  
     }
 
     private void AbrirDiario()
     {
-        textoDaNarrativa.SetActive(false); // Esconder texto da narrativa
+      //  textoDaNarrativa.SetActive(false); // Esconder texto da narrativa
         diarioCanvas.SetActive(true); // Mostrar o Canvas do diário
         diarioAberto = true; // Atualizar estado do diário
     }
@@ -42,7 +52,7 @@ public class Trigger_4 : MonoBehaviour
     {
         diarioCanvas.SetActive(false); // Esconder o Canvas do diário
         diarioAberto = false; // Atualizar estado do diário
-        StartCoroutine(ativaProximopasso()); // Avançar para o próximo passo
+        //StartCoroutine(ativaProximopasso()); // Avançar para o próximo passo
     }
 
     private IEnumerator ativaProximopasso()
@@ -51,13 +61,35 @@ public class Trigger_4 : MonoBehaviour
         // Aqui você pode adicionar a lógica para avançar no estado da narrativa ou qualquer outra ação necessária
     }
 
+
+private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+          //  gameObject.GetComponent<SphereCollider>().enabled = false; // Desativa o collider para não repetir a ativação
+           // textoDaNarrativa.GetComponent<TextMeshProUGUI>().text = meuTexto; // Atualiza o texto da narrativa
+          //  textoDaNarrativa.SetActive(true); // Ativa o texto da narrativa
+      
+         btnFeedback.GetComponent<Image>().enabled=false;
+         estaColindo = false;
+
+        
+        }
+    }
+
+
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            gameObject.GetComponent<BoxCollider>().enabled = false; // Desativa o collider para não repetir a ativação
-            textoDaNarrativa.GetComponent<TextMeshProUGUI>().text = meuTexto; // Atualiza o texto da narrativa
-            textoDaNarrativa.SetActive(true); // Ativa o texto da narrativa
+          //  gameObject.GetComponent<SphereCollider>().enabled = false; // Desativa o collider para não repetir a ativação
+           // textoDaNarrativa.GetComponent<TextMeshProUGUI>().text = meuTexto; // Atualiza o texto da narrativa
+          //  textoDaNarrativa.SetActive(true); // Ativa o texto da narrativa
+      
+         btnFeedback.GetComponent<Image>().enabled=true;
+              estaColindo = true;
+        
         }
     }
 }
